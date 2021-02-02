1a648271d01f97d70c306c45d3102ba3e1c0b70c
elletech <mygit_yutasugao20171126@yahoo.co.jp>
2021-02-02 13:49:15 +0000

    prettier eslint設定完了

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended', // defaultのルールを使用
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
