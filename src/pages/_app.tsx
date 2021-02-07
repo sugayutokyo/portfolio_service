e6193137c6a33bb145d289cbadd7563dae0c6cc9
elletech <mygit_yutasugao20171126@yahoo.co.jp>
2021-02-07 10:01:36 +0000

    next.jsでMaterial UIを扱う際に'Prop className did not match'が出てしまう不具合対応(SSRが原因)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { AppProps } from 'next/app'
 import { store } from '@/store/store'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from '@/styles/theme'
 import '@/styles/global.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  React.useEffect(() => {
+    // MaterialUIをSSRで扱う際に、MaterialUIで生成されたCSSを一度削除する
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   )
 }
