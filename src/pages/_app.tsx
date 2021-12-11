5841609cbf98a7085d64807281e89722dad16dbe
須加尾勇太 <wingmanair@sugabiyuutas-MacBook-Air.local>
2021-12-11 22:48:58 +0900

    changed to tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,27 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { AppProps } from 'next/app';
 import { store } from '@/store/store';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from '@/styles/theme';
 import '@/styles/global.scss';
 import Header from '@/components/Header';
 import 'tailwindcss/tailwind.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  React.useEffect(() => {
-    // MaterialUIをSSRで扱う際に、MaterialUIで生成されたCSSを一度削除する
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Header />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Header />
+      <Component {...pageProps} />
     </Provider>
   );
 };
